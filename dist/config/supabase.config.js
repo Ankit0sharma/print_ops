2db7b46e2e457c3b791f85e6fa03eaad1520b845
Ankit0sharma <ankitsharma@bitcot.com>
2025-04-12 15:45:17 +0530

    taking from .env

--- dist/config/supabase.config.js
+++ dist/config/supabase.config.js
@@ -13,13 +13,13 @@ exports.SupabaseService = void 0;
 const common_1 = require("@nestjs/common");
 const config_1 = require("@nestjs/config");
 const supabase_js_1 = require("@supabase/supabase-js");
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)({ path: `.env.${process.env.NODE_ENV}` });
+// import { config as dotenvConfig } from 'dotenv';
+// dotenvConfig({ path: `.env.${process.env.NODE_ENV}` });
 let SupabaseService = class SupabaseService {
     constructor(configService) {
         this.configService = configService;
-        this.supabaseClient = (0, supabase_js_1.createClient)("https://ppbgiifhlxofnrtzltzj.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwYmdpaWZobHhvZm5ydHpsdHpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyODAzNjksImV4cCI6MjA1OTg1NjM2OX0.yJZubpnf6U6XhA3GRBsvTp3hd1K_BoJ041PxMe-9Yaw");
-        this.supabaseAdmin = (0, supabase_js_1.createClient)("https://ppbgiifhlxofnrtzltzj.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwYmdpaWZobHhvZm5ydHpsdHpqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NDI4MDM2OSwiZXhwIjoyMDU5ODU2MzY5fQ.bGYw1i7cigV-IusEABjBc_PxW5ebvMumxbKGrnMugUc");
+        this.supabaseClient = (0, supabase_js_1.createClient)(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+        this.supabaseAdmin = (0, supabase_js_1.createClient)(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_KEY);
         console.log('Supabase URL:-------------');
     }
     getClient() {
--- src/config/supabase.config.ts
+++ src/config/supabase.config.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { config as dotenvConfig } from 'dotenv';
+// import { config as dotenvConfig } from 'dotenv';
 
-dotenvConfig({ path: `.env.${process.env.NODE_ENV}` });
+// dotenvConfig({ path: `.env.${process.env.NODE_ENV}` });
 @Injectable()
 export class SupabaseService {
   private supabaseClient: SupabaseClient;
@@ -11,13 +11,13 @@ export class SupabaseService {
 
   constructor(private configService: ConfigService) {
     this.supabaseClient = createClient(
-      "https://ppbgiifhlxofnrtzltzj.supabase.co",
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwYmdpaWZobHhvZm5ydHpsdHpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyODAzNjksImV4cCI6MjA1OTg1NjM2OX0.yJZubpnf6U6XhA3GRBsvTp3hd1K_BoJ041PxMe-9Yaw"
+      process.env.SUPABASE_URL,
+      process.env.SUPABASE_ANON_KEY
     );
 
     this.supabaseAdmin = createClient(
-      "https://ppbgiifhlxofnrtzltzj.supabase.co",
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwYmdpaWZobHhvZm5ydHpsdHpqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NDI4MDM2OSwiZXhwIjoyMDU5ODU2MzY5fQ.bGYw1i7cigV-IusEABjBc_PxW5ebvMumxbKGrnMugUc"
+      process.env.SUPABASE_URL,
+      process.env.SUPABASE_SERVICE_KEY
     );
 
     console.log('Supabase URL:-------------'); 
